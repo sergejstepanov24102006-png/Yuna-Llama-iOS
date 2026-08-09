125a42c9793351c90122c14cad735b5dfed5b236
sergejstepanov24102006-png <sergejstepanov24102006@gmail.com>
2026-08-09 13:59:21 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     products: [
         .library(
             name: "LlamaSwift",
-            targets: ["LlamaSwift"]
+            targets: ["LlamaSwift", "llama-cpp"]
         )
     ],
     targets: [
